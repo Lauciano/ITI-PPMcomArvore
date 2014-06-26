101d40a81fe34578b38e049ecee161d1e7fad849
Joanacelle <Joanacelle@hotmail.com>
2014-06-26 19:55:00 -0300

    versãoFinal

--- src/Leitor.java
+++ src/Leitor.java
@@ -1,107 +1,88 @@
+
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Leitor {
 
-	private BufferedReader br;
-	private String linhaAtual;
-	private int index;
-	private String nomeArq;
-	private boolean segMetade;
-	
-	public Leitor(String nomeArq){
-		
-		linhaAtual = "";
-		index = 0;
-		this.nomeArq = nomeArq;
-		segMetade = false;
-		
-		try{
-			br = new BufferedReader(new FileReader(nomeArq));
-			linhaAtual = br.readLine();
-			System.out.println("Linha lida = " + linhaAtual);
-		}catch(FileNotFoundException e){
-			System.err.println("Arquivo não encontrado!");
-		} catch (IOException e) {
-			System.err.println("Erro na leitura do arquivo!");
-		}
-	}
-	
-	public ArrayList<Byte> getAlfabeto(){
-		String linha;
-		ArrayList<Byte> alfabeto = new ArrayList<Byte>();
-		
-		try {
-			
-			BufferedReader br2 = new BufferedReader(new FileReader(nomeArq));
-			for(int i = 0; (linha = br2.readLine()) != null; i++){
-				for(int j = 0; j < linha.length(); j++){
-					char c = linha.charAt(j);
-					byte b = (byte) (c >> 8);
-					if(!alfabeto.contains(b)){
-						alfabeto.add(b);
-					}
-					b = (byte) (c & 0xff);
-					if(!alfabeto.contains(b)){
-						alfabeto.add(b);
-					}
-				}
-			}
-			br2.close();
-			
-		} catch (FileNotFoundException e) {
-			System.err.println("Arquivo não encontrado!");
-		} catch (IOException e){
-			System.err.println("Erro na leitura do arquivo!");
-		}
-		
-		
-		
-		return alfabeto;
- 		
-	}
-	
-	public Byte getNextByte(){
-		
-		//System.out.println("Entrei!");
-		
-		try {
-			br.ready();
-		} catch (IOException e1) {
-			System.err.println("Arquivo ja foi fechado!");
-			return null;
-		}
-		
-		if(index == linhaAtual.length()){
-			index = 0;
-			try {
-				linhaAtual = br.readLine();
-				if(linhaAtual == null){
-					br.close();
-					return null;
-				}
-			} catch (IOException e) {
-				System.out.println("Erro na leitura do arquivo!");
-			}
+    private DataInputStream data;
+    private byte[] simbolo;
+    private static boolean proximo = true;
+    private static byte bitfinal = -1;
+    private static int bitfinal_aux = 0;
+    private static int index = 0;
+    private String nomeArq;
 
-		}
-		
-		char c = linhaAtual.charAt(index);
-		byte b;
-		
-		if(segMetade){
-			b = (byte) (c & 0xff);
-			segMetade = false;
-			index++;
-		} else {
-			b = (byte) (c >> 8);
-			segMetade = true;
-		}
-		
-		return b;
-	}
-	
+    public Leitor(String nomeArq) throws FileNotFoundException, IOException {
+
+        this.nomeArq = nomeArq;
+        data = new DataInputStream(new BufferedInputStream(new FileInputStream(nomeArq)));
+        simbolo = new byte[5];
+
+    }
+
+    public ArrayList<Byte> getAlfabeto() throws FileNotFoundException, IOException {
+
+        int i, j;
+        byte[] simbolo_aux = new byte[5];
+        ArrayList<Byte> alfabeto = new ArrayList<Byte>();
+
+        DataInputStream data_aux = new DataInputStream(new BufferedInputStream(new FileInputStream(nomeArq)));
+
+        while ((i = data_aux.read(simbolo_aux)) > -1) {
+            for (j = 0; j < i; j++) {
+                if (!alfabeto.contains(simbolo_aux[j])) {
+                    alfabeto.add((byte) (simbolo_aux[j] & 0xff));
+                }
+
+            }
+        }
+
+        Collections.sort(alfabeto);
+
+        data_aux.close();
+
+        return alfabeto;
+
+    }
+
+    public Byte getNextByte() throws IOException {
+
+        if (proximo) {
+            if ((bitfinal = (byte) data.read(simbolo)) > -1) {
+                index = -1;
+                proximo = false;
+            } else {
+                data.close();
+                return null;
+            }
+        }
+        
+        if (index < 3) {
+            if (bitfinal != 5) {
+                if (bitfinal_aux < bitfinal) {
+                    bitfinal_aux++;
+                    return simbolo[bitfinal_aux];
+                } else {
+                    return null;
+                }
+            }
+            index++;
+            System.out.println((char) simbolo[index]);
+            return simbolo[index];
+            
+        }
+        index++;
+        proximo = true;
+        System.out.println((char) simbolo[index]);
+        
+        return simbolo[index];
+        
+
+    }
 }
